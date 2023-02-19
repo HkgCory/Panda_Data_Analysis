04d5cb24591c392c0f9699cf80226261491a1ebb
HkgCory <hkgkenneth@gmail.com>
2023-02-19 12:41:38 -0500

    panads-test

---
+++ env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/kinhanglo/Desktop/machine-learning/Panda_Data_Analysis/env/lib"
+XML2_LIBS="-lxml2 -L/Users/kinhanglo/Desktop/machine-learning/Panda_Data_Analysis/env/lib -lz -L/Users/kinhanglo/Desktop/machine-learning/Panda_Data_Analysis/env/lib -llzma -lpthread -L/Users/kinhanglo/Desktop/machine-learning/Panda_Data_Analysis/env/lib -liconv -L/Users/kinhanglo/Desktop/machine-learning/Panda_Data_Analysis/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/Users/kinhanglo/Desktop/machine-learning/Panda_Data_Analysis/env/include/libxml2 -I/Users/kinhanglo/Desktop/machine-learning/Panda_Data_Analysis/env/include"
+MODULE_VERSION="xml2-2.9.14"
+
